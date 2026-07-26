8c4a1d85a42e9b2d3bd7892e9af9edf5074f0cdd
ashshamiami-blip <ashshamiami@gmail.com>
2026-07-26 15:34:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+Ganti/upload 5 foto kamu ke folder ini dengan nama persis seperti berikut:
+
+- foto1.jpg
+- foto2.jpg
+- foto3.jpg
+- foto4.jpg
+- foto5.jpg
+- qris.jpg   (gambar kode QRIS kamu, untuk bagian Amplop Digital)
+
+Setelah upload, hapus file BACA-INI.txt ini (opsional, tidak wajib).
+
+Tips:
+- Kompres foto dulu di tinypng.com atau squoosh.app supaya web tidak berat (idealnya di bawah 500KB per foto).
+- Kalau nama file foto kamu beda (misal .png atau nama lain), edit tag <img src="..."> di index.html bagian galeri.
